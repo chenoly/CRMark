5bdef66e99b88d9f9b0c250f9884012964a8e130
chenoly <chenoly@foxmail.com>
2025-05-13 12:58:17 +0800

    LICENSE

--- crmark/__init__.py
+++
@@ -1,4 +0,0 @@
-from .crmark import CRMark
-
-__name__ = "crmark"
-__version__ = "0.0.1"
--- crmark/compressor/__init__.py
+++
@@ -1,3 +0,0 @@
-from .rdh import CustomRDH
-from .arithmeticcoder import CustomArithmeticEncoder
-from .utils_compressors import TensorCoder
